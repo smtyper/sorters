888d95f72cfa05f50e6d7b13d0daec84bebc30ed
Artem <smtyper@yandex.ru>
2021-05-04 00:57:00 +0300

    Create data generator

---
+++ SortingResearch/DataGenerator.cs
@@ -0,0 +1,26 @@
+﻿using RandomDataGenerator.FieldOptions;
+using RandomDataGenerator.Randomizers;
+
+namespace SortingResearch
+{
+    public class DataGenerator
+    {
+        private readonly IRandomizerBytes _randomizerBytes;
+        private readonly IRandomizerDateTime _randomizerDateTime;
+        private readonly IRandomizerGuid _randomizerGuid;
+        private readonly IRandomizerString _randomizerString;
+
+        public DataGenerator()
+        {
+            _randomizerString = RandomizerFactory.GetRandomizer(new FieldOptionsText
+            {
+                UseSpecial = false,
+                Min = 4,
+                Max = 16
+            });
+            _randomizerBytes = RandomizerFactory.GetRandomizer(new FieldOptionsBytes { Min = 0, Max = 9 });
+            _randomizerDateTime = RandomizerFactory.GetRandomizer(new FieldOptionsDateTime { IncludeTime = true });
+            _randomizerGuid = RandomizerFactory.GetRandomizer(new FieldOptionsGuid());
+        }
+    }
+}
---
+++ SortingResearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SortingResearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<DataGenerator>();
+                });
+    }
+}
