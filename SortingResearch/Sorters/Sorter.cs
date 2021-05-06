255e10c141068c7ba3a5000eb40eac12ab32f739
Artem <smtyper.contact@gmail.com>
2021-05-06 19:39:38 +0300

    Add sorter abstraction

---
+++ SortingResearch/Sorters/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingResearch.Sorters
+{
+    public abstract class Sorter
+    {
+        public T[] GetSortedCopy<T>(T[] sourceArray) where T : IComparable<T>
+        {
+            var resultArray = sourceArray.ToArray();
+            Sort(resultArray);
+
+            return resultArray;
+        }
+
+        protected abstract void Sort<T>(T[] array) where T : IComparable<T>;
+    }
+}
