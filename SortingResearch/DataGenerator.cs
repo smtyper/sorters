565c2cffdcb9c97cfec8690b1d165b29861ad42d
Artem <smtyper.contact@gmail.com>
2021-05-06 19:38:45 +0300

    Fix randomizers options

--- SortingResearch/DataGenerator.cs
+++ SortingResearch/DataGenerator.cs
@@ -1,26 +1,29 @@
-﻿using RandomDataGenerator.FieldOptions;
+﻿using System;
+using System.Linq;
+using RandomDataGenerator.FieldOptions;
 using RandomDataGenerator.Randomizers;
 
 namespace SortingResearch
 {
     public class DataGenerator
     {
-        private readonly IRandomizerBytes _randomizerBytes;
+        private readonly IRandomizerNumber<byte> _randomizerByte;
         private readonly IRandomizerDateTime _randomizerDateTime;
-        private readonly IRandomizerGuid _randomizerGuid;
+        private readonly IRandomizerNumber<int> _randomizerInteger;
         private readonly IRandomizerString _randomizerString;
 
         public DataGenerator()
         {
+            _randomizerByte = RandomizerFactory.GetRandomizer(new FieldOptionsByte { Min = 0, Max = 9 });
+            _randomizerInteger = RandomizerFactory.GetRandomizer(new FieldOptionsInteger
+            {
+                Min = int.MinValue, Max = int.MaxValue
+            });
             _randomizerString = RandomizerFactory.GetRandomizer(new FieldOptionsText
             {
-                UseSpecial = false,
-                Min = 4,
-                Max = 16
+                UseSpecial = false, Min = 8, Max = 16
             });
-            _randomizerBytes = RandomizerFactory.GetRandomizer(new FieldOptionsBytes { Min = 0, Max = 9 });
             _randomizerDateTime = RandomizerFactory.GetRandomizer(new FieldOptionsDateTime { IncludeTime = true });
-            _randomizerGuid = RandomizerFactory.GetRandomizer(new FieldOptionsGuid());
         }
     }
 }
