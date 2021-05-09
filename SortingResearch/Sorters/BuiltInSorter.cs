9efc994eacc2341cda8f0b074aa892ce15b4275e
Artem <smtyper@yandex.ru>
2021-05-09 12:08:55 +0300

    Implement built in sorter

---
+++ SortingResearch/Sorters/BuiltInSorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortingResearch.Sorters
+{
+    public class BuiltInSorter : Sorter
+    {
+        protected override T[] Sort<T>(T[] array)
+        {
+            Stopwatch.Restart();
+
+            Array.Sort(array);
+
+            Stopwatch.Stop();
+
+            return array;
+        }
+    }
+}
