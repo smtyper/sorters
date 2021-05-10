1c363f470a85d64efe3d8f0f9438ca4f3bd95a89
Artem <smtyper@yandex.ru>
2021-05-10 11:40:54 +0300

    Simplify program file

--- SortingResearch/Program.cs
+++ SortingResearch/Program.cs
@@ -1,47 +1,37 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SortingResearch;
 using SortingResearch.Sorters;
 
-namespace SortingResearch
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
+
+var host = CreateHostBuilder(args).Build();
+var researcher = host.Services.GetService<Researcher>();
+
+await researcher.ResearchAsync();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
         {
-            var host = CreateHostBuilder(args).Build();
+            services.AddSingleton<DataGenerator>().AddOptions<DataGeneratorSettings>()
+                .Bind(hostContext.Configuration.GetSection("DataGenerator"))
+                .ValidateDataAnnotations();
 
-            var researcher = host.Services.GetService<Researcher>();
-            await researcher.ResearchAsync();
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+            services.AddSingleton<ShellSorter>();
+            services.AddSingleton<QuickSorter>();
+            services.AddSingleton<MergeSorter>();
+            services.AddSingleton<HeapSorter>();
+            services.AddSingleton<RadixSorter>().AddOptions<RadixSorterSettings>()
+                .Configure(options =>
                 {
-                    services.AddSingleton<DataGenerator>().AddOptions<DataGeneratorSettings>()
-                        .Bind(hostContext.Configuration.GetSection("DataGenerator"))
-                        .ValidateDataAnnotations();
+                    var dataGeneratorSection = hostContext.Configuration.GetSection("DataGenerator");
 
-                    services.AddSingleton<ShellSorter>();
-                    services.AddSingleton<QuickSorter>();
-                    services.AddSingleton<MergeSorter>();
-                    services.AddSingleton<HeapSorter>();
-                    services.AddSingleton<RadixSorter>().AddOptions<RadixSorterSettings>()
-                        .Configure(options =>
-                        {
-                            var dataGeneratorSection = hostContext.Configuration.GetSection("DataGenerator");
-
-                            options.MaxIntegerRank = dataGeneratorSection["IntegerMax"].Length;
-                            options.MaxStringRank = int.Parse(dataGeneratorSection["StringMaxLength"]);
-                        });
-                    services.AddSingleton<BuiltInSorter>();
-
-                    services.AddSingleton<Researcher>().AddOptions<ResearcherSettings>()
-                        .Bind(hostContext.Configuration.GetSection("Researcher"))
-                        .ValidateDataAnnotations();
+                    options.MaxIntegerRank = dataGeneratorSection["IntegerMax"].Length;
+                    options.MaxStringRank = int.Parse(dataGeneratorSection["StringMaxLength"]);
                 });
-    }
-}
+            services.AddSingleton<BuiltInSorter>();
+
+            services.AddSingleton<Researcher>().AddOptions<ResearcherSettings>()
+                .Bind(hostContext.Configuration.GetSection("Researcher"))
+                .ValidateDataAnnotations();
+        });
