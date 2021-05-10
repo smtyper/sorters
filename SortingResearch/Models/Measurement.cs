978ff9c722f679267f736433f557c591ba63f2fc
Artem <smtyper.contact@gmail.com>
2021-05-10 11:00:09 +0300

    Add measurement model

---
+++ SortingResearch/Models/Measurement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortingResearch.Models
+{
+    public class Measurement
+    {
+        public string SorterName { get; init; }
+
+        public ArrayGenerationType ArrayGenerationType { get; init; }
+
+        public TypeCode ArrayType { get; init; }
+
+        public int ArrayLength { get; init; }
+
+        public TimeSpan Elapsed { get; init; }
+    }
+}
