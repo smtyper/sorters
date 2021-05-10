6a5f2c63a8c7d65e39cf7d818fd0da67add1ece2
Artem <smtyper.contact@gmail.com>
2021-05-10 11:00:21 +0300

    Add aggregation model

---
+++ SortingResearch/Models/Aggregation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper.Configuration;
+
+namespace SortingResearch.Models
+{
+    public class Aggregation
+    {
+        public string SorterName { get; init; }
+
+        public Dictionary<int, TimeSpan> ElapsedTimes { get; init; }
+    }
+}
